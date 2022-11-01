937b5196b013ddc30f8c057b504b633f54dbe926
Chris Moss <hcmoss70@gmail.com>
2022-10-31 23:10:53 -0400

    input and type practice

---
+++ typeConversion.py
@@ -0,0 +1,15 @@
+birth_year = input("Enter your birth year: ")
+
+age = 2022 - int(birth_year)
+
+print("Your age is: " + str(age))
+
+int()
+float()
+bool()
+str()
+
+num_1 = float(input("First: "))
+num_2 = float(input("Second: "))
+
+print("Sum: " + str(num_1 + num_2))
