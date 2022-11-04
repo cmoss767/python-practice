45e95a4e59e6c055ff54da022cefc41433149dcb
Chris Moss <hcmoss70@gmail.com>
2022-11-04 00:25:16 -0400

    for and while loops

---
+++ forLoops.py
@@ -0,0 +1,13 @@
+nums = [1, 2, 3, 4, 5]
+
+# each element is assigned to item
+
+for item in nums:
+    print(item)
+
+# while loop is a little more verbose in this case
+# better to use for loop
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
---
+++ whileLoops.py
@@ -0,0 +1,10 @@
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+while i <= 5:
+    # repeats the string based off the number
+    print(i * '*')
+    i += 1
