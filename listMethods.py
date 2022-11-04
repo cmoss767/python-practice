8c280fc59038a59b91f3533503b149f4a43ec11c
Chris Moss <hcmoss70@gmail.com>
2022-11-04 00:18:38 -0400

    lists

---
+++ listMethods.py
@@ -0,0 +1,18 @@
+nums = [1, 2, 3, 4, 5]
+
+# adds at end
+nums.append(6)
+
+# adds -1 at index 0
+nums.insert(0, -1)
+
+nums.remove(3)
+
+# removes all elements in list
+nums.clear()
+
+# checking to see if 1 is in list--boolean
+print(1 in nums)
+
+# number of elements in the list
+print(len(nums))
---
+++ lists.py
@@ -0,0 +1,12 @@
+names = ["John", "Bob", "Mosh", "Sam", "Mary"]
+# index of the first element in the list
+print(names[0])
+# index of the last element in the list
+print(names[-1])
+
+# changing variable at index
+names[0] = "Jon"
+
+# prints elements at 0-2 index
+# returns a new list, doesnt modify
+print(names[0:3])
