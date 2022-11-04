db648c0c049d3d94c9e3f74def12687493a99ad6
Chris Moss <hcmoss70@gmail.com>
2022-11-04 00:31:03 -0400

    tuples

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 age = 20
 print(age)
+
+# returns a range object
+# need to iterate over to show the nums
+nums = range(5)
+# only shows the assignment
+print(nums)
+
+
+for num in nums:
+    print(num)
+
+# shows 5-9
+newNums = range(5, 10)
+
+# shows 5-9 with a stepper of 2
+newNums = range(5, 10, 2)
+
+# can also do this
+for num in range(5):
+    print(num)
---
+++ tuples.py
@@ -0,0 +1,7 @@
+# like lists but cannot be changed
+# doesnt have the same methods as list
+
+nums = (1, 2, 3)
+
+# does not work
+nums[0] = 10
