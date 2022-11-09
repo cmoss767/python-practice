3ce15612f6496023ee24f22a4a8de9750368a3f6
Chris Moss <hcmoss70@gmail.com>
2022-11-08 20:56:11 -0500

    added first beginner project

---
+++ beginnerProjects/Madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# string concatenation
+
+# a few ways to do this
+# youtuber = "Chris"
+
+# print("subscribee to " + youtuber)
+# print("subscribe to {".format(youtuber))
+
+# print(f"subcribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+mablib = f"Computer programing is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydraded and {verb2} like you are {famous_person}"
+
+print(mablib)
